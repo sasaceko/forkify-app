634fc251f1e0391111c116cc9244043deebfea63
sasaceko <ceko.sasa@gmail.com>
2021-05-03 13:32:19 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3f5dbda9-d4ad-45c2-8ed5-8ee6a907f3d7';
+export const MODAL_CLOSE_SEC = 2.5;
