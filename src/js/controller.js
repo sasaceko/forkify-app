9052be7eb69b928ef3e0f99ca649f49033fbd2bd
sasaceko <ceko.sasa@gmail.com>
2021-05-03 13:41:13 +0200

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,6 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  console.log('New feature');
 };
 init();
